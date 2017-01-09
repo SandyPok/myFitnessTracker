414c0de1d71368f558da3d7b354a83c4486b046a
“SandyPok” <Pok.Sandy@yahoo.com>
2017-01-09 10:50:10 -0500

    Update

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users do
+    resources :goals
+  end
   resources :goals
-  
+
   root "welcome#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
